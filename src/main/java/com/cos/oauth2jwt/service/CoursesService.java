95c79d8a9cbb9f32e0589d9401cdb369d1a413d0
LEEJAECHEOL <jae_choel@naver.com>
2021-03-18 01:19:56 +0900

    add save courses, jsonConverter

--- src/main/java/com/cos/oauth2jwt/service/CoursesService.java
+++ src/main/java/com/cos/oauth2jwt/service/CoursesService.java
@@ -1,8 +1,22 @@
 package com.cos.oauth2jwt.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.cos.oauth2jwt.domain.courses.Courses;
+import com.cos.oauth2jwt.domain.courses.CoursesRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
 public class CoursesService {
-
+	
+	private final CoursesRepository coursesRepository;
+	
+	@Transactional
+	public Courses 저장하기(Courses courses) {
+		return coursesRepository.save(courses);
+	}
+	
 }
--- src/main/java/com/cos/oauth2jwt/web/courses/CoursesRestController.java
+++ src/main/java/com/cos/oauth2jwt/web/courses/CoursesRestController.java
@@ -1,11 +1,43 @@
 package com.cos.oauth2jwt.web.courses;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cos.oauth2jwt.domain.courses.Courses;
+import com.cos.oauth2jwt.service.CoursesService;import com.cos.oauth2jwt.web.dto.CMRespDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
 public class CoursesRestController {
-
+	
+	private final CoursesService coursesService;
+	
+	@GetMapping("/courses")
+	public String findById() {
+		return "test";
+	}
+	
+	@PostMapping("/courses")
+	public CMRespDto<?> save(@RequestBody Courses courses) {
+		System.out.println(courses);
+		Courses coursesEntity =  coursesService.저장하기(courses);
+		if(coursesEntity != null){
+			return new CMRespDto<>(HttpStatus.CREATED.value(), "success" , null);
+		}else {
+			return new CMRespDto<>(HttpStatus.BAD_REQUEST.value(), "fail" , null);
+		}
+		
+	}
 }
