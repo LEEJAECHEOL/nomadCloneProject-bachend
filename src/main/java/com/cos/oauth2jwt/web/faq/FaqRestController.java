c8b690239c3f984be5b5a7784377d8e9301f3252
unknown <qwq140@gmail.com>
2021-03-17 17:53:14 +0900

    faq 전체찾기, 상세보기

--- src/main/java/com/cos/oauth2jwt/web/faq/FaqRestController.java
+++ src/main/java/com/cos/oauth2jwt/web/faq/FaqRestController.java
@@ -1,11 +1,41 @@
 package com.cos.oauth2jwt.web.faq;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cos.oauth2jwt.domain.faq.Faq;
+import com.cos.oauth2jwt.service.FaqService;
+import com.cos.oauth2jwt.web.faq.dto.FaqSaveReqDto;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
 public class FaqRestController {
-
+	private final FaqService faqService;
+	
+	@GetMapping("/faq")
+	public List<Faq> findAll() {
+		List<Faq> faqs = faqService.전체찾기();
+		return faqs;
+	}
+	
+	@GetMapping("/faq/{id}")
+	public Faq findById(@PathVariable long id) {
+		Faq faqEntity = faqService.상세보기(id);
+		return faqEntity;
+	}
+	
+	@PostMapping("/faq")
+	public Faq save(@RequestBody FaqSaveReqDto faqSaveReqDto) {
+		Faq faq = faqSaveReqDto.toEntity();
+		Faq faqEntity = faqService.저장하기(faq);
+		return faqEntity;
+		
+	}
 }
