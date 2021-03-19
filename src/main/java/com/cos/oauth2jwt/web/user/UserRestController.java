f681c4922170f5b4f41bc0552a81b4856480f6e1
unknown <qwq140@gmail.com>
2021-03-19 15:14:58 +0900

    CMRespDto

--- src/main/java/com/cos/oauth2jwt/web/user/UserRestController.java
+++ src/main/java/com/cos/oauth2jwt/web/user/UserRestController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cos.oauth2jwt.config.auth.PrincipalDetails;
 import com.cos.oauth2jwt.domain.user.User;
 import com.cos.oauth2jwt.service.UserService;
+import com.cos.oauth2jwt.web.dto.CMRespDto;
 import com.cos.oauth2jwt.web.user.dto.UserUpdateReqDto;
 
 import lombok.RequiredArgsConstructor;
@@ -22,20 +23,21 @@ public class UserRestController {
 
 	private final UserService userService;
 	
-	// email, name 수정
+	// edit profile (email, name)
 	@PutMapping("/user/{id}")
-	public int findById(@PathVariable Long id, @RequestBody UserUpdateReqDto userUpdateReqDto, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+	public CMRespDto<?> findById(@PathVariable Long id, @RequestBody UserUpdateReqDto userUpdateReqDto, @AuthenticationPrincipal PrincipalDetails principalDetails) {
 		User userEntity = userService.프로필수정(id, userUpdateReqDto);
 		principalDetails.setUser(userEntity);
-		return 1;
+		return new CMRespDto<>(1,"성공",null);
 	}
-	
-	// 프로필이미지 변경
 	
 	// 회원탈퇴
 	@DeleteMapping("/user/{id}")
-	public int deleteById(@PathVariable Long id) {
+	public CMRespDto<?> deleteById(@PathVariable Long id) {
 		userService.회원탈퇴(id);
-		return 1;
+		return new CMRespDto<>(1,"성공",null);
 	}
+	
+	// image update
+	
 }
