7685ab6eb28515ae895199e4b735dd598a5b9c9d
LEEJAECHEOL <jae_choel@naver.com>
2021-03-30 00:54:24 +0900

    모델 수정

--- src/main/java/com/cos/oauth2jwt/domain/pay/Pay.java
+++ src/main/java/com/cos/oauth2jwt/domain/pay/Pay.java
@@ -26,12 +26,14 @@ public class Pay {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String pay_method;  	// 결제수단
+	private String imp_uid;			// 고유주문번호
 	private String merchant_uid;	// 고유 주문번호
-	private int amount;				// 가격
+	private String amount;			// 가격
 	private String name;			// 주문명
+	private String pg_tid;			// 카드사 승인번호
 	private String buyer_name;		// 구매자명
-	private String buyer_tel;		// 구매자 번호
 	private String buyer_email;		// 구매자 이메일
+	private String status;			// 상태
 	
 	@ManyToOne
     @JoinColumn(name = "couseId")
