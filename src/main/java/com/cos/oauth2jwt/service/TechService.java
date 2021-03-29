5da3cc6ed33cc9b1716253555ea8db5a53c464f3
unknown <qwq140@gmail.com>
2021-03-29 20:39:51 +0900

    Tech 모델, 레파지토리, 컨트롤러 생성, Tech-Courses 관계 사이 모델 생성

---
+++ src/main/java/com/cos/oauth2jwt/service/TechService.java
@@ -0,0 +1,49 @@
+package com.cos.oauth2jwt.service;
+
+import java.util.List;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cos.oauth2jwt.domain.tech.Tech;
+import com.cos.oauth2jwt.domain.tech.TechRepository;
+import com.cos.oauth2jwt.web.tech.dto.TechSaveReqDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class TechService {
+
+	private final TechRepository techRepository;
+	
+	@Transactional(readOnly = true)
+	public List<Tech> 테크전체찾기() {
+		return techRepository.findAll();
+	}
+	
+	@Transactional
+	public void 테크저장(TechSaveReqDto techSaveReqDto) {
+		
+		// 이미지 업로드 해야함
+		
+		Tech tech = techSaveReqDto.toEntity();
+		techRepository.save(tech);
+	}
+	
+	
+	@Transactional
+	public void 테크삭제(long id) {
+		techRepository.deleteById(id);
+	}
+	
+	@Transactional
+	public void 테크수정(long id, TechSaveReqDto techSaveReqDto) {
+		
+		// 이미지 업로드 해야함
+		
+		Tech techEntity = techRepository.findById(id).get();
+		techEntity.setName(techSaveReqDto.getName());
+	}
+}
