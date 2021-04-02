9354ddde52bdbc132b856cf0420e98091af48dde
LEEJAECHEOL <jae_choel@naver.com>
2021-04-02 19:54:54 +0900

    tech 수정

--- src/main/java/com/cos/oauth2jwt/domain/TechCourses/TechCoursesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cos.oauth2jwt.domain.TechCourses;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TechCoursesRepository extends JpaRepository<TechCourses, Long>{
-
-}
